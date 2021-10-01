b85988cf2fbf9ad7388ea0239d910ab1b54700fe
rtre84 <rtre84@users.noreply.github.com>
2021-10-01 18:41:31 -0400

    test

---
+++ _site/rollup.config.js
@@ -0,0 +1,67 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonJs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
+import { terser } from "rollup-plugin-terser";
+import dts from 'rollup-plugin-dts';
+import { name, homepage, version, dependencies, peerDependencies } from './package.json';
+
+const umdConf = {
+  format: 'umd',
+  name: 'Globe',
+  globals: { react: 'React' },
+  banner: `// Version ${version} ${name} - ${homepage}`
+};
+
+export default [
+  {
+    external: ['react'],
+    input: 'src/index.js',
+    output: [
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }), // To fool React in the browser
+      babel({ exclude: '**/node_modules/**', babelHelpers: 'bundled' }),
+      resolve(),
+      commonJs()
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/index.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`,
+        exports: 'auto'
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)],
+    plugins: [
+      babel({ babelHelpers: 'bundled' })
+    ]
+  },
+  { // expose TS declarations
+    input: 'src/index.d.ts',
+    output: [{
+      file: `dist/${name}.d.ts`,
+      format: 'es'
+    }],
+    plugins: [dts()]
+  }
+];
---
+++ _site/src/index.js
@@ -0,0 +1,33 @@
+import fromKapsule from 'react-kapsule';
+import GlobeKapsule from 'globe.gl';
+import GlobePropTypes from './globe-proptypes';
+
+const Globe = fromKapsule(
+  GlobeKapsule,
+  {
+    methodNames: [ // bind methods
+      'pauseAnimation',
+      'resumeAnimation',
+      'pointOfView',
+      'scene',
+      'camera',
+      'renderer',
+      'postProcessingComposer',
+      'controls',
+      'getCoords',
+      'getScreenCoords',
+      'toGeoCoords',
+      'toGlobeCoords'
+    ],
+    initPropNames: [
+      'animateIn',
+      'waitForGlobeReady',
+      'rendererConfig'
+    ]
+  }
+);
+
+Globe.displayName = 'Globe';
+Globe.propTypes = GlobePropTypes;
+
+export default Globe;
